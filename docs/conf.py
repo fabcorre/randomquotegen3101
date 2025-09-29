f0ca04744afc7664365b34ea62a42d3e60e692be
Fabian Correa <facorrea@gmail.com>
2025-09-29 18:41:42 -0400

    Ready for first release

--- docs/conf.py
+++ docs/conf.py
@@ -21,7 +21,7 @@ release = "0.1.0"
 # https://www.sphinx-doc.org/en/master/usage/configuration.html#general-configuration
 
 extensions = [
-    'sphinx.ext.autodoc',
+    "sphinx.ext.autodoc",
 ]
 
 templates_path = ["_templates"]
